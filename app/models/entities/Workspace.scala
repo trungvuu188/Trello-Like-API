2d39eecdec5691589530c63617c25eecc972364b
Vu Tran Trung <vutt1808@gmail.com>
2025-08-13 09:37:10 +0700

    URL services
    # Conflicts:
    #	conf/routes

--- app/models/entities/Workspace.scala
+++ app/models/entities/Workspace.scala
@@ -4,20 +4,20 @@ import models.Enums
 import java.time.LocalDateTime
 
 case class Workspace(
-                      id: Option[Int] = None,
-                      name: Option[String] = None,
-                      status: Enums.WorkspaceStatus = Enums.Active,
-                      createdBy: Option[Int] = None,
-                      createdAt: Option[LocalDateTime] = None,
-                      updatedAt: Option[LocalDateTime] = None
-                    )
+    id: Option[Int] = None,
+    name: Option[String] = None,
+    status: Enums.WorkspaceStatus = Enums.Active,
+    createdBy: Option[Int] = None,
+    createdAt: Option[LocalDateTime] = None,
+    updatedAt: Option[LocalDateTime] = None
+)
 
 case class UserWorkspace(
-                          id: Option[Int] = None,
-                          userId: Option[Int] = None,
-                          workspaceId: Option[Int] = None,
-                          role: Option[Enums.UserWorkspaceRole] = None,
-                          status: Enums.UserWorkspaceStatus = Enums.WorkspaceActive,
-                          invitedBy: Option[Int] = None,
-                          joinedAt: Option[LocalDateTime] = None
-                        )
+    id: Option[Int] = None,
+    userId: Option[Int] = None,
+    workspaceId: Option[Int] = None,
+    role: Option[Enums.UserWorkspaceRole] = None,
+    status: Enums.UserWorkspaceStatus = Enums.WorkspaceActive,
+    invitedBy: Option[Int] = None,
+    joinedAt: Option[LocalDateTime] = None
+)
