31eab8a1606de7730acbbc9bc0549813c9ca0182
tuannguyenh1 <tuannguyenh1@nashtechglobal.com>
2025-08-22 14:06:42 +0700

    2332

--- app/services/CookieService.scala
+++ app/services/CookieService.scala
@@ -27,7 +27,6 @@ class CookieService @Inject()(config: Configuration) {
             value = token,
             maxAge = Some(maxAge),
             path = "/",
-            domain = None, // Will use current domain
             secure = secure,
             httpOnly = httpOnly,
             sameSite = sameSite
@@ -40,7 +39,6 @@ class CookieService @Inject()(config: Configuration) {
             value = "",
             maxAge = Some(0), // Expire immediately
             path = "/",
-            domain = None,
             secure = secure,
             httpOnly = httpOnly,
             sameSite = sameSite
